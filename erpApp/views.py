c8b109664b5ad571ff82ba5b9381f397b268f0cc
sangram <mail.sangram.desai@gmail.com>
2022-08-09 16:05:58 +0530

    first commit

---
+++ erpApp/views.py
@@ -0,0 +1,26 @@
+from django.views.generic import ListView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from .models import Person,City
+from django.shortcuts import render
+
+class PersonListView(ListView):
+    model = Person
+    template_name = 'erpApp/person_list.html'
+    context_object_name = 'people'
+
+class PersonCreateView(CreateView):
+    model = Person
+    template_name = 'erpApp/person_form.html'
+    fields = ('name', 'birthdate', 'country', 'city')
+    success_url = reverse_lazy('person_changelist')
+
+class PersonUpdateView(UpdateView):
+    model = Person
+    template_name = 'erpApp/person_form.html'
+    fields = ('name', 'birthdate', 'country', 'city')
+    success_url = reverse_lazy('person_changelist')
+    
+def load_cities(request):
+    country_id = request.GET.get('country')
+    cities = City.objects.filter(country_id=country_id).order_by('name')
+    return render(request, 'erpApp/city_dropdown_list_options.html', {'cities': cities})    
